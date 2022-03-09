1205c3323764f56c2c37d597e9aff79e2b4ddcf5
captainkent <29131214+captainkent@users.noreply.github.com>
2022-03-09 11:09:13 +0100

    Updated NewBank to make use of new command style. Added singleton instances of CustomerManager and CommandManager.

--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -1,55 +1,62 @@
 package newbank.server;
 
-import java.util.HashMap;
+import newbank.server.commands.Command;
+import newbank.server.commands.CommandException;
+import newbank.server.commands.CommandManager;
+import newbank.server.commands.CommandResponse;
+
+import java.util.ArrayList;
 
 public class NewBank {
 	
 	private static final NewBank bank = new NewBank();
-	private HashMap<String,Customer> customers;
+	private CustomerManager theCustomerManager = CustomerManager.getInstance();
+	private CommandManager theCommandManager = CommandManager.getInstance();
 	
-	private NewBank() {
-		customers = new HashMap<>();
-		addTestData();
-	}
-	
-	private void addTestData() {
-		Customer bhagy = new Customer();
-		bhagy.addAccount(new Account("Main", 1000.0));
-		customers.put("Bhagy", bhagy);
-		
-		Customer christina = new Customer();
-		christina.addAccount(new Account("Savings", 1500.0));
-		customers.put("Christina", christina);
-		
-		Customer john = new Customer();
-		john.addAccount(new Account("Checking", 250.0));
-		customers.put("John", john);
-	}
-	
+	private NewBank() { }
+
 	public static NewBank getBank() {
 		return bank;
 	}
 	
 	public synchronized CustomerID checkLogInDetails(String userName, String password) {
-		if(customers.containsKey(userName)) {
+		if(theCustomerManager.validateLogin(userName, password)) {
 			return new CustomerID(userName);
 		}
 		return null;
 	}
 
 	// commands from the NewBank customer are processed in this method
-	public synchronized String processRequest(CustomerID customer, String request) {
-		if(customers.containsKey(customer.getKey())) {
-			switch(request) {
-			case "SHOWMYACCOUNTS" : return showMyAccounts(customer);
-			default : return "FAIL";
+	public synchronized String processRequest(CustomerID customerId, ArrayList<String> anArgsList) {
+		String myResponse = "Unknown customer";
+		CommandResponse myCommandResponse = null;
+		Command myCommand = null;
+
+		Customer myCustomer = theCustomerManager.getCustomer(customerId);
+
+		if(myCustomer != null) {
+			try {
+				// First item in list is command key.
+				myCommand = theCommandManager.getCommand(anArgsList.get(0));
+
+				if (myCommand != null) {
+					System.out.println("Processing command.");
+
+					// Remove our processed command, remaining items are the arguments.
+					anArgsList.remove(0);
+					myCommandResponse = myCommand.process(anArgsList);
+
+				} else {
+					myResponse = "Unknown command!";
+				}
+
+			} catch (CommandException e) {
+				myCommandResponse = myCommand.usage();
+				System.err.println(e.getMessage());
 			}
 		}
-		return "FAIL";
-	}
-	
-	private String showMyAccounts(CustomerID customer) {
-		return (customers.get(customer.getKey())).accountsToString();
-	}
+		myResponse = myCommandResponse.getResponse();
 
+		return myResponse;
+	}
 }
