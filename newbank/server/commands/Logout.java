e6d8e65555b478575e79092566e30b3a7a05e381
Georgeh712 <86979254+Georgeh712@users.noreply.github.com>
2022-03-17 18:30:51 +0000

    Move login to separate method in NewBankClientHandler

--- newbank/server/commands/Logout.java
+++ newbank/server/commands/Logout.java
@@ -1,14 +1,26 @@
 package newbank.server.commands;
 
+import newbank.server.NewBankClientHandler;
+import newbank.server.NewBankServer;
+import newbank.server.customers.CustomerID;
+
 import java.util.ArrayList;
 
 public class Logout implements Command {
     public CommandResponse process(ArrayList<String> anArgsList) throws CommandException {
-        if (anArgsList.size() < 0) {
-            throw new CommandException("Invalid arguments!");
+        if (anArgsList.size() != 1) {
+
+            String myException = "Invalid arguments, anArgsList contains: ";
+            myException = myException.concat(anArgsList.toString());
+            throw new CommandException(myException);
+        } else {
+
+
+            String myResponse = "Logout Successful";
+
+            return new CommandResponse(myResponse);
         }
 
-        return new CommandResponse("");
     };
 
     public CommandResponse usage() {
