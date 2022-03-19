1bd35b6956126476c004445e018a9b1b1d592890
captainkent <29131214+captainkent@users.noreply.github.com>
2022-03-19 13:54:30 +0100

    Added AccountID and AccountIDTest to test it.

---
+++ test/newbank/server/accounts/AccountIDTest.java
@@ -0,0 +1,41 @@
+package newbank.server.accounts;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+class AccountIDTest {
+    private ArrayList<AccountID> theAccountIds;
+
+    @BeforeEach
+    void setUp() {
+        theAccountIds = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Account myAccount = new Account(String.format("Test%d", i));
+            theAccountIds.add(new AccountID(myAccount));
+        }
+    }
+
+    @Test
+    void fixedAccountIdLength() {
+        int accountIdLength = 2 + 12;   // 2 for the NR characters, 12 for the number, including padding.
+
+        for (int i = 0; i < theAccountIds.size(); i++) {
+            Assertions.assertEquals(accountIdLength, theAccountIds.get(i).getAccountID().length());
+        }
+    }
+
+    @Test
+    void noIdenticalAccountIds() {
+        // Test that no account IDs are the same.
+        for (int i = 0; i < theAccountIds.size(); i++) {
+            for (int j = i + 1; j < theAccountIds.size(); j++) {
+                String firstAccount = theAccountIds.get(i).getAccountID();
+                String secondAccount = theAccountIds.get(j).getAccountID();
+                Assertions.assertFalse(firstAccount.equals(secondAccount));
+            }
+        }
+    }
+}
