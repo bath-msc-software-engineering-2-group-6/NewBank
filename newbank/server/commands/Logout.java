1f42c920661e5623f739eccd4748994ea2427ca0
Georgeh712 <86979254+Georgeh712@users.noreply.github.com>
2022-03-20 21:33:08 +0000

    Added some javadocs for NewBankClientHandler and Logout

--- newbank/server/commands/Logout.java
+++ newbank/server/commands/Logout.java
@@ -3,7 +3,11 @@ package newbank.server.commands;
 import java.util.ArrayList;
 
 public class Logout implements Command {
-
+    /**
+     * Processes command for logging out
+     * @param anArgsList - List of arguments from user input
+     * @return CommandResponse, response from the logout process
+     */
     public CommandResponse process(ArrayList<String> anArgsList) throws CommandException {
         if (anArgsList.size() != 1) {
 
@@ -17,7 +21,10 @@ public class Logout implements Command {
         }
 
     };
-
+    /**
+     * Displays to the user how to use the command
+     * @return CommandResponse, instructions on how to use command as a String
+     */
     public CommandResponse usage() {
         return new CommandResponse("Type \"LOGOUT\"");
     }
